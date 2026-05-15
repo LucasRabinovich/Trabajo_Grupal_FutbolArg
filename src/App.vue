4be9ddc31b907c6c33c1d9327ebb52402b8f36fc
rosarioconde <conde.rosario88@gmail.com>
2026-05-14 21:34:46 -0300

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,9 @@
+<script setup>
+import NavBar from './components/navBar.vue';
+
+</script>
 
 <template>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">CRUD Vue Router</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <RouterLink class="nav-link" to="/">Publicaciones</RouterLink>
-          </li>
-          <li class="nav-item">
-            <RouterLink class="nav-link" to="/usuarios">Usuarios</RouterLink>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <NavBar />
   <RouterView />
 </template>
