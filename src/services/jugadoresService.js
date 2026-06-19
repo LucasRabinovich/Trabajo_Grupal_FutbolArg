69289dce4611493aa77b11a1d6308cf1dbfba0b4
santinomartinezmerli <45015381@comunidadort.edu.ar>
2026-06-18 21:13:32 -0300

    Detalles jugadores

--- src/services/jugadoresService.js
+++ src/services/jugadoresService.js
@@ -8,29 +8,40 @@ const cliente = axios.create({
   headers: { 'Content-Type': 'application/json' }
 })
 
+const getId = (jugador) => {
+  return jugador.id || jugador.jugadores
+}
+
+const normalizar = (jugador) => {
+  if (!jugador.id) {
+    jugador.id = jugador.jugadores
+  }
+  return jugador
+}
+
 const obtenerJugadores = async () => {
   const respuesta = await cliente.get('/jugadores')
-  return respuesta.data
+  return (respuesta.data || []).map(normalizar)
 }
 
 const obtenerJugadorPorId = async (id) => {
   const respuesta = await cliente.get(`/jugadores/${id}`)
-  return respuesta.data
+  return normalizar(respuesta.data)
 }
 
 const crearJugador = async (jugador) => {
   const respuesta = await cliente.post('/jugadores', jugador)
-  return respuesta.data
+  return normalizar(respuesta.data)
 }
 
 const actualizarJugador = async (id, jugador) => {
   const respuesta = await cliente.put(`/jugadores/${id}`, jugador)
-  return respuesta.data
+  return normalizar(respuesta.data)
 }
 
 const eliminarJugador = async (id) => {
   const respuesta = await cliente.delete(`/jugadores/${id}`)
-  return respuesta.data
+  return normalizar(respuesta.data)
 }
 
 export {
